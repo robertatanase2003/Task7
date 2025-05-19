e4d782c2e3176532fc0c9dc9075681f76a2f3df3
robertatanase2003 <tanase.roberta2003@gmail.com>
2025-05-19 07:15:59 +0300

    updated Marketing class to use SafeVideoPlayer proxy

--- Task7_CTS/src/en/ase/sqt/texteditor/flyweight/abstracts/Displayable.java
+++
@@ -1,7 +0,0 @@
-package en.ase.sqt.texteditor.flyweight.abstracts;
-
-import en.ase.sqt.texteditor.flyweight.models.CharacterContext;
-
-public interface Displayable {
-    void display(CharacterContext var1);
-}
